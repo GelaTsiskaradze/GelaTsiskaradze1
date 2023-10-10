56bd82664b3469b77b4549920c9937cb56ac82bf
GelaTsiskaradze <tsiskaradzee@gmail.com>
2023-10-10 21:59:12 +0400

    Finishied HW04 (Task1)

---
+++ Homework04/Task1/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sheiyvanet masivis zoma: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Elementi {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Sheiyvanet indeqsi: ");
+            int index = int.Parse(Console.ReadLine());
+
+            if (index < 0 || index >= size)
+            {
+                Console.WriteLine("Sheiyvanet Validuri indeqsi");
+                return;
+            }
+
+            int element = array[index];
+            int sum = 0;
+
+            // Dajamebis funqcia
+            while (element != 0)
+            {
+                sum += element % 10;
+                element /= 10;
+            }
+
+            Console.WriteLine($"Elementi {index} cifrta jami: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
